0b26db842b0afd6e755586906b7b9c63db72c447
Seankps <seanmclements@gmail.com>
2019-05-30 23:33:14 -0400

    test

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+    api.cache(true);
+  
+    const presets = [
+        "@babel/preset-env"
+    ];
+    const plugins = [
+        "@babel/plugin-proposal-class-properties"
+    ];
+  
+    return {
+      presets,
+      plugins
+    };
+  }
